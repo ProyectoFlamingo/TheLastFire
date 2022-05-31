d63590dcf2f9005b8e726992b4072945c17c5cb8
Lif Gwaethrakindo <metallicasnake.cruyff14@gmail.com>
2022-05-30 21:17:07 -0500

    Tested that both Destino & Moskar were playable. Gonna fix the cape's weights and try to re-parametrize Shanty
     // Lif

---
+++ Assets/Scripts/Controllers/BlackboardManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Voidless;
+
+namespace Flamingo
+{
+public class BlackboardManager : Singleton<BlackboardManager>
+{
+	private Blackboard<string, HashSet<Projectile>> _projectilesBlackboards;
+}
+}
