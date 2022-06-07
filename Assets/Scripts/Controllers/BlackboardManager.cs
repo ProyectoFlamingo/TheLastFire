8f6b998c9eecc331f7a19bda40c1909f519e52e6
Lif Gwaethrakindo <metallicasnake.cruyff14@gmail.com>
2022-06-06 19:53:29 -0500

    General clean-up part 1. Next one is from Animation assets, but I need to use all AnimatorControllers to see which animations ought to be deprecated.
     // Lif

--- Assets/Scripts/Controllers/BlackboardManager.cs
+++ Assets/Scripts/Controllers/BlackboardManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +9,19 @@ namespace Flamingo
 public class BlackboardManager : Singleton<BlackboardManager>
 {
 	private Blackboard<string, HashSet<Projectile>> _projectilesBlackboards;
+
+	/// <summary>Gets and Sets projectilesBlackboards property.</summary>
+	public static Blackboard<string, HashSet<Projectile>> projectilesBlackboards
+	{
+		get { return Instance._projectilesBlackboards; }
+		set { Instance._projectilesBlackboards = value; }
+	}
+
+	private static void AddProjectile(string _key, Projectile _projectile)
+	{
+		/*if(projectilesBlackboards == null) projectilesBlackboards = new Blackboard<string, HashSet<Projectile>>();
+		if(projectilesBlackboards.ContainsKey)*/
+
+	}
 }
 }
